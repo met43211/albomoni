4b08d289e925ec8701ca2ba696e6df9d4e0b783c
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-04-02 01:55:24 +0300

    feat: suspense to category header

--- src/page/category/ui/index.tsx
+++ src/page/category/ui/index.tsx
@@ -1,4 +1,6 @@
 import { AdsList } from '@albomoni/widgets/ads-list/ui';
+import { Suspense } from 'react';
+import { Skeleton } from '@nextui-org/react';
 import { CategoryHeader } from './category-header';
 
 type Props = {
@@ -8,7 +10,16 @@ type Props = {
 
 export const CategoryPage = ({ lng, categoryId }: Props) => (
   <main className='flex flex-col gap-10 items-center'>
-    <CategoryHeader lng={lng} categoryId={categoryId} />
+    <Suspense
+      fallback={
+        <div className='max-w-7xl w-full px-4'>
+          <Skeleton className='w-full h-52 rounded-3xl' />
+        </div>
+      }
+    >
+      <CategoryHeader lng={lng} categoryId={categoryId} />
+    </Suspense>
+
     <div className='w-full max-w-7xl px-4 flex flex-col-reverse lg:flex-row gap-6'>
       <AdsList title='Недавние объявления' cols={4} />
       {/* <div className='flex flex-col gap-8 w-96 '>
