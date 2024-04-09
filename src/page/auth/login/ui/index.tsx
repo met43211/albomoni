5319b77e309e7d43c56cb94f6737dd4a039313b3
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-04-09 15:26:23 +0300

    fix: remove useRouter in login

--- src/page/auth/login/ui/index.tsx
+++ src/page/auth/login/ui/index.tsx
@@ -1,37 +1,32 @@
 import { LoginWidget } from '@albomoni/features/auth/login';
 import { AlbomoniSvg } from '@albomoni/shared/ui/logo';
-import { Button } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 import { Suspense } from 'react';
 import { PiXBold } from 'react-icons/pi';
 
 export const LoginPage = () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/');
-  };
-
   return (
     <main className='flex flex-col gap-6 items-center px-4 md:justify-center min-h-dvh light:md:bg-[--element] pb-4'>
       <div className='w-full md:absolute top-0 flex justify-between p-1 pt-6 md:p-10'>
-        <button
-          type='button'
-          aria-label='Logo'
-          onClick={handleClick}
-          className='w-[132px] flex items-center justify-center'
-        >
-          <AlbomoniSvg />
-        </button>
-        <Button
-          onPress={handleClick}
-          radius='full'
-          size='md'
-          isIconOnly
-          className='light:md:bg-[--bg]'
-        >
-          <PiXBold size={20} />
-        </Button>
+        <Link href='/'>
+          <button
+            type='button'
+            aria-label='Logo'
+            className='w-[132px] flex items-center justify-center'
+          >
+            <AlbomoniSvg />
+          </button>
+        </Link>
+
+        <Link href='/'>
+          <button
+            type='button'
+            aria-label='Back'
+            className='w-10 h-10 rounded-full bg-[--bg] dark:bg-[--element] flex items-center justify-center'
+          >
+            <PiXBold size={20} />
+          </button>
+        </Link>
       </div>
 
       <Suspense>
