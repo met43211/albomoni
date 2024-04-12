48df12dab7da36df5624cf8e7818701d4d72bf02
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-04-12 16:59:29 +0300

    fix: permanent redirect in unauth

--- src/app/[lng]/unauth/page.tsx
+++ src/app/[lng]/unauth/page.tsx
@@ -2,14 +2,12 @@
 
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 import { useCookies } from 'react-cookie';
 
 export default function UnAuth() {
   const [_cookies, _setCookie, removeCookie] = useCookies();
-  const router = useRouter();
 
   removeCookie('token');
-  
-  router.push('/');
+  permanentRedirect('/');
 }
