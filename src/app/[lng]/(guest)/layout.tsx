2a6a6521206ad65c0bfb0f622781c3322836aafe
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-04-18 18:15:26 +0300

    fix: auth wrapper

--- src/app/[lng]/(guest)/layout.tsx
+++ src/app/[lng]/(guest)/layout.tsx
@@ -1,16 +1,22 @@
-import { cookies } from 'next/headers';
-import { permanentRedirect } from 'next/navigation';
+'use client';
+
+import { useValidateToken } from '@albomoni/shared/api/use-validate-token';
+import { Spinner } from '@nextui-org/spinner';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function GuestLayout({ children }: Props) {
-  const token = cookies().get('token');
+  const { isLogged, isPending } = useValidateToken();
 
-  if (token?.value) {
-    permanentRedirect('/login');
+  if (isPending) {
+    return (
+      <div className='fixed top-0 bottom-0 right-0 left-0 flex justify-center items-center'>
+        <Spinner />
+      </div>
+    );
   }
 
-  return children;
+  return !isLogged ? children : <>Go away</>;
 }
--- src/app/[lng]/(header)/(account)/layout.tsx
+++ src/app/[lng]/(header)/(account)/layout.tsx
@@ -1,16 +1,22 @@
-import { cookies } from 'next/headers';
-import { permanentRedirect } from 'next/navigation';
+'use client';
+
+import { useSession } from '@albomoni/shared/lib/hooks/use-session';
+import { Spinner } from '@nextui-org/spinner';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function AuthLayout({ children }: Props) {
-  const token = cookies().get('token');
+  const { isPending, isLogged } = useSession();
 
-  if (!token?.value) {
-    permanentRedirect('/login');
+  if (isPending) {
+    return (
+      <div className='absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center'>
+        <Spinner />
+      </div>
+    );
   }
 
-  return children;
+  return isLogged ? children : <>Go away</>;
 }
